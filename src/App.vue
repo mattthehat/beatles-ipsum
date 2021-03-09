007f37dc88fcfc6531563db2bc25b668d0c3ccf2
Matt <pihundredpounds@gmail.com>
2021-03-09 08:05:49 +0000

    Fixed paragraphs

---
+++ src/App.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Beatles Ipsum</h1>
+    </header>
+    <div class="ipsum-options">
+      <form @submit.prevent="generateIpsum">
+        <div>
+          <label for="paragraphs">Number of Paragraphs:</label>
+          <input id="paragraphs" type="number" v-model="paragraphs">
+        </div>
+        <button>Generate Ipsum</button>
+      </form>
+    </div>
+    <template v-if="showIpsum">
+      <div class="ipsum-results">
+        <p v-for="p in parseInt(paragraphs)" :key="p" >
+          {{generateRandomSentence()}}
+        </p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import {phrases} from './data/phrases';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      phrases,
+      paragraphs: 6,
+      showIpsum: false,
+      sentenceLengthMin: 10,
+      sentenceLengthMax: 14
+    }
+  },
+  methods: {
+    getRandomPhrase() {
+      return this.phrases[Math.floor(Math.random() * this.phrases.length)]
+    },
+    generateRandomSentence() {
+      let words = '';
+      let paragraph = '';
+      const numSentences = 4;
+      const sentenceLength = Math.floor(Math.random() * (this.sentenceLengthMax - this.sentenceLengthMin) + this.sentenceLengthMin);
+
+
+      for(let i = 1; i < sentenceLength; i++) {
+        words += this.getRandomPhrase() + ' ';
+      }
+
+      words = words.charAt(0).toUpperCase() + words.slice(1)
+
+      for(let i = 1; i < (Math.floor(Math.random() * numSentences) + 2); i ++) {
+        paragraph += (words.trim().endsWith('?') ? words.trim() : words.trim() + '. ')
+      }
+
+      return paragraph;
+
+    },
+    generateIpsum() {
+      this.showIpsum = false
+      this.showIpsum = true
+    }
+  },
+  computed: {
+    sentence() {
+      return this.generateRandomSentence()
+    }
+  }
+  
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Limelight&display=swap');
+
+body {
+  min-width: 100vw;
+  min-height: 100vh;
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  font-family: 'Open Sans', sans-serif;
+  background:#000;
+  color: #fff;
+}
+
+header {
+  width:100%;
+  text-align: center;
+  color:#000;
+  margin-bottom: 24px;
+  padding: 35px 24px;
+  color: #fff;
+  box-sizing: border-box;
+}
+
+header h1 {
+  font-size: 5em;
+  font-family: 'Limelight', cursive;
+}
+
+#app {
+  background:#000;
+  width:100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+  font-size: inherit;
+  font-family: inherit;
+  border: none;
+  padding: 12px 24px;
+  cursor: pointer;
+  background: #fff;
+  color:#000;
+  display: block;
+  margin-top: 35px;
+}
+
+label {
+  margin-right: 30px;
+}
+
+.ipsum-options {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24px;
+}
+
+input {
+  font-size: inherit;
+}
+
+.ipsum-options form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.ipsum-results {
+  padding: 24px;
+  box-sizing: border-box;
+}
+</style>
